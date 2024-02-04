be16f0d49f5f3ef26e1120b3ec8e8f7bdfcc4d00
Guilherme Ferreira <guil.ferreiram@gmail.com>
2024-02-04 08:19:01 -0300

    Interagindo com Nuget

--- Curso-Bibliotecas/ByteBank_GeradorChavePix/ByteBank_GeradorChavePix/GeradorPix.cs
+++ Curso-Bibliotecas/ByteBank_GeradorChavePix/ByteBank_GeradorChavePix/GeradorPix.cs
@@ -6,13 +6,27 @@ using System.Threading.Tasks;
 
 namespace ByteBank_GeradorChavePix
 {
+    /// <summary>
+    /// Classe que gera chaves Pix usando o formato GUID.
+    /// </summary>
     public static class GeradorPix
     {
+
+        /// <summary>
+        /// Método que retorna uma chave aleatória Pix.
+        /// </summary>
+        /// 
+        /// <returns>Retorna uma chave Pix no formato string.</returns>
         public static string GetChavePix()
         {
             return Guid.NewGuid().ToString();
         }
 
+        /// <summary>
+        /// Método que retorna uma lista aleatória de chaves Pix.
+        /// </summary>
+        /// <param name="numeroChaves">Quantidade de chaves a serem geradas.</param>
+        /// <returns>Uma lista de strings de chaves Pix.</returns>
         public static List<string> GetChavesPix(int numeroChaves)
         {
             if (numeroChaves <= 0)
